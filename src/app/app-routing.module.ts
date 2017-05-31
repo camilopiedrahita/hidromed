ac78c6fdc356ab85d1463275160bcf75f9192da0
Camilo Piedrahita Hernández <cpiedrahtiah@gmail.com>
2017-05-31 11:06:35 -0500

    Agregar componente home con su ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { TableroRapidoComponent } from './tablero-rapido/tablero-rapido.componen
 import { PerfilComponent } from './perfil/perfil.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'tablero-rapido', },
   { path: 'tablero-rapido', component:  TableroRapidoComponent },
   { path: 'mis-graficos', component: MisGraficosComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'perfil', component: PerfilComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { PerfilComponent } from './perfil/perfil.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     NavbarComponent,
     PerfilComponent,
     PageNotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
